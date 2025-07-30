56879d7bef2a0d689bc664af9eaaea8e90f2403f
cozykbin <70093181+cozykbin@users.noreply.github.com>
2025-07-30 15:54:02 +0900

    Update main.py
    test3

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
 
     # 저녁 알림 스케줄 (오후 5시 30분)
     scheduler.add_job(
-        send_meal_message, 'cron', day_of_week='mon-fri', hour=15, minute=50,
+        send_meal_message, 'cron', day_of_week='mon-fri', hour=15, minute=58,
         args=[webhook_url, 'dinner'], id='dinner_notification'
     )
     logging.info("저녁 메뉴 알림이 매주 월-금 17:30에 설정되었습니다.")
